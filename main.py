60466fb66a4dc4c4f7173a55a1c74cf1de7bcb2c
isiborella <isiborella98@gmail.com>
2025-01-15 15:33:12 +0100

    second commit

--- main.py
+++ main.py
@@ -1,33 +1,7 @@
 from flask import Flask,redirect,url_for,render_template,request
-
-app=Flask(__name__)
-@app.route('/',methods=['GET','POST'])
-def home():
-    power = 2**25
-
-    # Declare variables
-    firstfigure     = None
-    secondfigure    = None
-    answer          = None
-    if request.method == 'POST':
-        print(request.form)
-        firstfigure     = request.form['firstfigure']
-        secondfigure    = request.form['secondfigure']
-
-        firstfigure     = int(firstfigure)
-        secondfigure    = int(secondfigure)
-        answer          = firstfigure + secondfigure
-        
-    return render_template('index.html', power=power, firstfigure=firstfigure, secondfigure=secondfigure, answer=answer)
-
-@app.route('/emma')
-def emma():
-    return render_template('emma.html')
-
-@app.route('/nater')
-def nater():
-    return render_template('nater.html')
+import config
+from application import app
 
 if __name__ == '__main__':
     #DEBUG is SET to TRUE. CHANGE FOR PROD
-    app.run(port=5000,debug=True)
+    app.run(host=config.HOST,port=config.PORT,debug=True)
