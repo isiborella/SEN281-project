fd8dc78fd4a7a309221669ee4c6b96c4ec1fd75d
isiborella <isiborella98@gmail.com>
2025-01-18 10:10:50 +0100

    third commit

--- application/routes.py
+++ application/routes.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from random import random
 from flask import Flask,redirect,url_for,render_template,request
 from application import app
 
@@ -7,3 +9,21 @@ def home():
         # Handle POST Request here
         return render_template('index.html')
     return render_template('index.html')
+
+# Route for the Live Data Page
+@app.route('/live-data', methods=['GET'])
+def live_data():
+    # Simulate live network speed data
+    network_speed = random.randint(10, 100)  # Generate a random speed in Mbps
+    return render_template('live_data.html', speed=network_speed)
+
+# Route for the Historical Data Page
+@app.route('/historical-data', methods=['GET'])
+def historical_data():
+    # Simulate historical data as a list of dictionaries
+    historical_data = [
+        {"date": datetime.datetime.now().strftime("%Y-%m-%d"), "speed": random.randint(10, 100)},
+        {"date": (datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d"), "speed": random.randint(10, 100)},
+        {"date": (datetime.datetime.now() - datetime.timedelta(days=2)).strftime("%Y-%m-%d"), "speed": random.randint(10, 100)},
+    ]
+    return render_template('historical_data.html', data=historical_data)
